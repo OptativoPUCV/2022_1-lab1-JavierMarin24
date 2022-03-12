6c438688d7c0a46a2b70690402dd623a29617172
Your Name <you@example.com>
2022-03-11 23:26:53 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -87,8 +87,8 @@ Vector * crearVector(int n) {
    //frases = (tipoLetra *) realloc (frases, n+1 * sizeof(tipoLetra));
    Vector *arreglo = NULL;
    arreglo = (Vector *) realloc (arreglo,  (sizeof(Vector)));
-   arreglo->datos = (int *) realloc(arreglo->datos, n*(sizeof(int)));
-   //arreglo->capacidad=n;
+   arreglo->datos = (int *) calloc(n*(sizeof(int)));
+   arreglo->capacidad=n;
    return arreglo;
 }
 
